5d1c2638992aec6ada483d60014710180cc834b7
JMG <92858147+Gepzuu@users.noreply.github.com>
2024-07-04 10:52:27 +0800

    Refactored main function, added JSON marshalling and printing of scraped items

--- main.go
+++ main.go
@@ -12,31 +12,37 @@ type Item struct {
 	Instock string `json:"instock"`
 }
 
-
 func main() {
-	c :=colly.NewCollector()
+	c := colly.NewCollector()
 
-	items :=[]Item{}
+	items := []Item{}
 
 	c.OnHTML("li.next a", func(h *colly.HTMLElement) {
 		c.Visit(h.Request.AbsoluteURL(h.Attr("href")))
 
 	})
 
-	c.OnHTML("article.product_pod", func(h *colly.HTMLElement){
-		i := Item {
-		Link: h.ChildAttr("a", "href"),
-		Name: h.ChildAttr("h3 a", "title"),
-		Price: h.ChildText("p.price_color"),
-		Instock: h.ChildText("p.instock"),
-
-	}
-	items = append(items, i)
+	c.OnHTML("article.product_pod", func(h *colly.HTMLElement) {
+		i := Item{
+			Link:    h.ChildAttr("a", "href"),
+			Name:    h.ChildAttr("h3 a", "title"),
+			Price:   h.ChildText("p.price_color"),
+			Instock: h.ChildText("p.instock"),
+		}
+		items = append(items, i)
 	})
 
-	c.OnRequest(func(r *colly.Request){
+	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
 	c.Visit("https://books.toscrape.com/catalogue/page-1.html")
+	
+	data, err := json.MarshallIndent(items, " ", "")
+	if err!= nil {
+        log.Fatal()
+    }
+
+	fmt.Println(string(data))
+
 }
